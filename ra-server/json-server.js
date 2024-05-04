5189d202912c0e9dab118ef64e43e71315d04e46
Vishwas <vishwas.g5@gmail.com>
2024-05-04 15:06:54 +0530

    Setup json-server

---
+++ ra-server/json-server.js
@@ -0,0 +1,23 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running on port 3000");
+});
